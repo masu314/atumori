d1d41041d73e279eb713485a163db69b816d3af1
増田怜奈 <masudareina@masudareinanoMacBook-Air.local>
2023-12-31 02:21:34 +0900

    [add]投稿編集ページのデザインを設定、投稿画像のプレビュー機能を追加
    /posts/edit.html.erbのhtmlを編集し、cssを適用。preview.jsを作成し、プレビュー機能を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,12 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application.scss"
+import "./preview"
 
 Rails.start()
 Turbolinks.start()
