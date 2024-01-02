22299caad9af309468f2e73070a195ef907528f5
masu314 <remasuda1188@gmail.com>
2024-01-02 16:08:13 +0900

    [add]フォロー機能を実装
    Relationshipモデルを作成、Relationshipsコントローラーを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :favorites, dependent: :destroy
   has_many :favorites_posts, through: :favorites, source: :post
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def favorite(post)
     favorites_posts << post
@@ -20,5 +24,17 @@ class User < ApplicationRecord
 
   def favorite?(post)
     favorites_posts.include?(post)
+  end
+
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
