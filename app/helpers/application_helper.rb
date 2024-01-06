6bd936888ee6ac7debbca2b9aa6e5627df5a0cdc
masu314 <remasuda1188@gmail.com>
2024-01-06 19:31:17 +0900

    [add]投稿一覧ページに検索ソート機能を追加
    helperメソッドとして、選択されたurlのソート部分のクエリパラメータを、渡した文字に置き換える独自メソッドを定義。posts/index.html.erbにselectフォームを作成し、各オプションのvalueに値を設定する。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
+  
   def header_categories
     categories = Category.all
     return categories
+  end
+
+  def sort_by_sort_url(url, order)
+    url.gsub(/%5Bs%5D=\w+\+\w+/, "%5Bs%5D=#{order}")
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
   has_many :users, through: :favorites
   belongs_to :category
   validates :user_id, {presence: true}
+  ransacker :favorites_count do
+    query = '(SELECT COUNT(favorites.post_id) FROM favorites where favorites.post_id = posts.id GROUP BY favorites.post_id)'
+    Arel.sql(query)
+  end
 end
