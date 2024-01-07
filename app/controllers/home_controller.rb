83fe40720c20e9e7737e5463008b242a4f4ced3f
増田怜奈 <masudareina@masudareinanoMacBook-Air.local>
2024-01-08 00:00:53 +0900

    [add]topページに新着投稿と人気投稿を表示
    homeコントローラのtopアクションの変数@newest_postsと@most_favorite_postsを定義、top.html.erbで@newest_postsと@favorite_postsをeach文で一つずつ取り出し表示する設定を記載

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def top
+    @newest_posts = Post.limit(6).order(updated_at: :desc)
+    @most_favorite_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(6).pluck(:post_id))
   end
 end
