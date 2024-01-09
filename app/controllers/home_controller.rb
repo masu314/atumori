44710bd3878192a04254148f6190bc1b4c1bfaa4
増田怜奈 <masudareina@masudareinanoMacBook-Air.local>
2024-01-10 00:55:34 +0900

    [update]topページにもっと見るボタンを設置,topページにもっと見るボタンを設置するにあたり並び替えボタンの実装方法を変更
    top.html.erbにてsearch_formを設置し、hiddenフィールドで新着sort,人気sortのデータを送る。submitボタンを「もっと見る」という文言にする。posts/index.html.erb にて記載していたセレクトタグを削除し、並び替え用のsearch_formをもう一つ作成。hiddenフィールドで検索ワードの情報を送る。helperに記載していたsort_by_sort_url(url, order)メソッドを削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def top
     @newest_posts = Post.limit(6).order(updated_at: :desc)
     @most_favorite_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(6).pluck(:post_id))
+    @q = Post.ransack(params[:q])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 module ApplicationHelper
-  
   def header_categories
     categories = Category.all
     return categories
-  end
-
-  def sort_by_sort_url(url, order)
-    url.gsub(/%5Bs%5D=\w+\+\w+/, "%5Bs%5D=#{order}")
   end
 end
