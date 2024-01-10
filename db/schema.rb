791f8a6e0385047bf802f54b7e3c367ce19852ce
masu314 <remasuda1188@gmail.com>
2024-01-10 12:07:24 +0900

    [remove]Categoryモデルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_02_130130) do
+ActiveRecord::Schema.define(version: 2024_01_09_164647) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2024_01_02_130130) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
   end
 
   create_table "favorites", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categpry1 = Category.create(name: '・お酒')
+water, beer, sake, wine = drink.children.create(
+  [
+    { name: '水・ソフトドリンク' },
+    { name: 'ビール・洋酒' },
+    { name: '日本酒・焼酎' }
+    { name: 'ワイン' }
+  ]
+)
+
+['水・ミネラルウォーター', 'コーヒー', '野菜・果実飲料', 'お茶・紅茶', '炭酸飲料', 'スポーツドリンク'].each do |name|
+  water.children.create(name: name)
+end
+
+['ビール・発泡酒', 'ウイスキー', 'チューハイ・ハイボール・カクテル'].each do |name|
+  beer.children.create(name: name)
+end
+
+%w[焼酎 日本酒 梅酒].each do |name|
+  sake.children.create(name: name)
+end
+
+['赤ワイン', '白ワイン', '飲み比べセット', 'スパークリングワイン・シャンパン'].each do |name|
+  wine.children.create(name: name)
+end
