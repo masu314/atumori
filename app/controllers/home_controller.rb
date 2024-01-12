3a7f72538e6605abddf1557d806200120e67251b
増田怜奈 <masudareina@masudareinanoMacBook-Air.local>
2024-01-12 10:18:28 +0900

    [update]topページにカテゴリーボタンを実装
    home_controller.rb の top アクションで @categories を定義し、親カテゴリーを格納。
    home/top.html.erb でeach文を記載し、@categoriesの値を一つずつ取り出す。取り出した値は、search_form_forで検索ページに送る。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @newest_posts = Post.limit(6).order(updated_at: :desc)
     @most_favorite_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(6).pluck(:post_id))
     @q = Post.ransack(params[:q])
+    @categories = Category.roots
   end
 end
