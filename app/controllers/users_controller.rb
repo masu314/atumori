fd4046e4b2582d90971ff5d3fcba228b59e9e307
増田怜奈 <masudareina@masudareinanoMacBook-Air.local>
2024-01-12 15:36:23 +0900

    [add]ユーザー一覧ページを追加、デザインを変更
    users コントローラーに index アクションを追加し、posts コントローラの index アクションと同様に、params[q]がある場合とない場合をif文で分け、@users に検索結果を格納。
    users/index.html.erb に search_fom_for で検索フォームを設置。
    ユーザーの一覧表示部分は、users/_user_list.html.erb に記載。each文で @users の値を一つずつ取り出し、user に格納。_user_list.html.erb は renderを使い、users/index.html.erb で読み込む。
    application_helper.rb に記載していた不要なメソッドを削除。
    その他のファイル変更は、デザインの変更。パンくずリストで使うクラス名を変更したり、フォロー件数、フォロワー件数に下線を引くなど。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+
+  def index
+    if params[:q].present?
+      @q = User.ransack(params[:q])
+      @users = @q.result(distinct: true)
+    else
+      params[:q] = { sorts: 'created_at desc' }
+      @q = User.ransack(params[:q])
+      @users = @q.result(distinct: true)
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = Post.where(user_id: @user.id)
