368ad57761dd56a891bb8f33abe1997e68fabc84
増田怜奈 <masudareina@masudareinanoMacBook-Air.local>
2024-01-15 01:38:04 +0900

    [update]投稿画面や投稿編集画面の項目にバリデーションを設定
    post.rbの各カラムにバリデーションを設定、posts/new.html.erb や posts/edit.html.erb にrequired属性や、pattern属性等を追加し、値を入力しないとメッセージが出るように設定。エラーが出たため、postsコントローラでbeforeアクションにupdateやcreateも指定。

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
   has_many :users, through: :favorites
   belongs_to :category
   validates :user_id, {presence: true}
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :image, presence: true
+  validates :category_id, presence: true
+  validates :work_id, presence: true, format: { with: /MO-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}\z/}
+  validates :author_id, presence: true, format: { with: /MA-[0-9]{4}-[0-9]{4}-[0-9]{4}\z/}
   
   ransacker :favorites_count do
     query = '(SELECT COUNT(favorites.post_id) FROM favorites where favorites.post_id = posts.id GROUP BY favorites.post_id)'
