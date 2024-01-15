d0039d8e5f5f7df15e36658bec3f4a0815af607a
masu314 <remasuda1188@gmail.com>
2024-01-16 04:00:49 +0900

    [fix]database.ymlを修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2024_01_10_040358) do
 
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -34,22 +34,22 @@ ActiveRecord::Schema.define(version: 2024_01_10_040358) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "favorites", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+  create_table "favorites", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_favorites_on_post_id"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2024_01_10_040358) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.string "title"
     t.string "image"
     t.text "text"
@@ -69,14 +69,14 @@ ActiveRecord::Schema.define(version: 2024_01_10_040358) do
     t.integer "category_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
+  create_table "relationships", charset: "utf8mb3", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,42 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# clothes = Category.create(name: '服')
-# caps = Category.create(name: '帽子')
-# face_paint = Category.create(name: 'フェイスペイント')
-# accessories = Category.create(name: '小物')
-# ground = Category.create(name: '地面')
-# furniture = Category.create(name: '家具')
-# others = Category.create(name: 'その他')
+clothes = Category.create(name: '服')
+caps = Category.create(name: '帽子')
+face_paint = Category.create(name: 'フェイスペイント')
+accessories = Category.create(name: '小物')
+ground = Category.create(name: '地面')
+furniture = Category.create(name: '家具')
+others = Category.create(name: 'その他')
 
-# ['シャツ', 'パーカー', 'セーター', 'アウター', 'ワンピース', 'ドレス', '着物', 'その他'].each do |name|
-#   clothes.children.create(name: name)
-# end
+['シャツ', 'パーカー', 'セーター', 'アウター', 'ワンピース', 'ドレス', '着物', 'その他'].each do |name|
+  clothes.children.create(name: name)
+end
 
-# ['つば付きキャップ', 'ニットキャップ', 'つば付きハット', 'その他'].each do |name|
-#   caps.children.create(name: name)
-# end
-
-
-# ['眉毛', '前髪', 'その他'].each do |name|
-#   face_paint.children.create(name: name)
-# end
+['つば付きキャップ', 'ニットキャップ', 'つば付きハット', 'その他'].each do |name|
+  caps.children.create(name: name)
+end
 
 
-# ['傘', 'うちわ', '手旗', 'スマホケース', 'その他'].each do |name|
-#   accessories.children.create(name: name)
-# end
+['眉毛', '前髪', 'その他'].each do |name|
+  face_paint.children.create(name: name)
+end
 
 
-# ['レンガ', 'タイル', '石畳', 'ウッドデッキ', '道路', 'ラグ', 'その他'].each do |name|
-#   ground.children.create(name: name)
-# end
+['傘', 'うちわ', '手旗', 'スマホケース', 'その他'].each do |name|
+  accessories.children.create(name: name)
+end
 
 
-# ['顔出し看板', '屋台', '看板', 'パネル', 'その他'].each do |name|
-#   furniture.children.create(name: name)
-# end
+['レンガ', 'タイル', '石畳', 'ウッドデッキ', '道路', 'ラグ', 'その他'].each do |name|
+  ground.children.create(name: name)
+end
+
+
+['顔出し看板', '屋台', '看板', 'パネル', 'その他'].each do |name|
+  furniture.children.create(name: name)
+end
 
 User.create!(name:'testuser', email: 'test@test', passward: "testtest")
 Post.create!(title: 'test', author_id: 'MA-1111-1111-1111', work_id: 'MO-A1A1-A1A1-A1A1, user_id: 1,
-category_id: 9')
+category_id: 9') 
