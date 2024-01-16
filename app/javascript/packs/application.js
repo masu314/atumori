00e487f05e9cfcb087711fda2fdb210d74ba9153
masu314 <remasuda1188@gmail.com>
2024-01-16 21:06:28 +0900

    [fix]activestorage にバリデーションを設定、プレビュー画面の不具合を修正
    gem activestroge-validatorをインストール。post.rbやuser.rb で5MB以上のファイルはアップロードできないようにする。posts/edit.html.erb や posts/new.html.erb や devise/registrations/edit.html.erb でscriptを設定し、そこでプレビューの処理を行う。
    その他カラムのバリデーションを追加。viewでaccept属性を設定し、指定したファイルのみ添付できるようにする。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application.scss"
-import "./preview"
 import "./category_post"
 import "./category_search"
 
