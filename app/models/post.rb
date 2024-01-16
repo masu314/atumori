50a6f87de591ef5e14128bb20cc7952442ce2d22
masu314 <remasuda1188@gmail.com>
2024-01-16 21:19:19 +0900

    [fix]IDとフレンドコードの正規表現を修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,8 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 20 }
   validates :category_id, presence: true
-  validates :work_id, presence: true, format: { with: /MO-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}\z/}
-  validates :author_id, presence: true, format: { with: /MA-[0-9]{4}-[0-9]{4}-[0-9]{4}\z/}
+  validates :work_id, presence: true, format: { with: /\AMO-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}\z/}
+  validates :author_id, presence: true, format: { with: /\AMA-[0-9]{4}-[0-9]{4}-[0-9]{4}\z/}
   validates :text, length: { maximum: 200 }
   validates :image, presence: true, size: { less_than: 5.megabytes, message: "は5MB以下である必要があります。" }
 
