51729c7afce4f021a225a5d4b48b3f1bd8104c65
masu314 <remasuda1188@gmail.com>
2024-01-24 20:27:51 +0900

    [add,fix]ゲストログイン機能を実装,READMEの画像を変更
    ゲストユーザーはプロフィールの変更、アカウントの削除ができないようにする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,4 +84,13 @@ class User < ApplicationRecord
       errors.add(:user_image, 'は5MB以下である必要があります。')
     end
   end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
