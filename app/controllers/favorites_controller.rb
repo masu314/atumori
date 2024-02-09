89ea780f7a8086c16009aa8d62a91b0fc7932e29
増田怜奈 <masudareina@masudareinanoMacBook-Air.local>
2024-02-10 03:01:14 +0900

    [fix]お気に入り機能でわかりにくい処理を修正
    コメントを追加、favorite?の箇所の処理を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,15 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  
   def create
     post = Post.find(params[:post_id])
     current_user.favorite(post)
-    redirect_back fallback_location: posts_path
+    redirect_to request.referer
   end
 
   def destroy
-    post = current_user.favorites_posts.find(params[:post_id])
+    post = current_user.favorite_posts.find(params[:post_id])
     current_user.unfavorite(post)
-    redirect_back fallback_location: posts_path
+    redirect_to request.referer
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: true
+  # ユーザーが同じ投稿に複数回お気に入り登録できないようにするためのバリデーション
   validates :user_id, uniqueness: { scope: :post_id }
 end
